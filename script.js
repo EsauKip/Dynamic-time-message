d8572c1dc8d361b711da60b641c55e86f0e26963
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-18 15:47:41 +0300

    select the message area from the dom

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 const date = new Date();
 // grt the hour from the date
 const hour = date.getHours();
+// select button from the DOM element
+const button = document.querySelector('button');
+// select the  message area from the dom element
