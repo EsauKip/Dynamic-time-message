cacd34588876dc05f2a7a1e8574af32352f7d69a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-18 16:12:16 +0300

    check the hour of the day

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ const hour = date.getHours();
 // select button from the DOM element
 const button = document.querySelector('button');
 // select the  message area from the dom element
-const message = document.querySelector('#message');
+const messageArea = document.querySelector('#message');
 //add click event to the button
 button.addEventListener('click', function() {
-    
+    // check the hour of the day if its noon ,evening or morning
+    if (hour>17){
+        messageArea.innerHTML =`<h1>Good Evening</h1>`;
+    }
+    else if (hour>12){
+        messageArea.innerHTML =`<h1>Good Afternoon</h1>`;
+    }
+    else {
+        messageArea.innerHTML =`<h1>Good Morning</h1>`;
+    }
+}
+)
