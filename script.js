5c1c4a7d0c0b83f79d201fcba553a1ca94679450
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-18 15:50:30 +0300

    add click event to the button

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const hour = date.getHours();
 // select button from the DOM element
 const button = document.querySelector('button');
 // select the  message area from the dom element
+const message = document.querySelector('#message');
+//add click event to the button
+button.addEventListener('click', function() {
+    
